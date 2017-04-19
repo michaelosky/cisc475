80e5feaa70b71a48e4ee2592e8f07ff4b7381b66
Rob Weber <robertweber95@gmail.com>
2017-04-19 18:13:03 -0400

    Disable right-click to print

--- web/viewer.js
+++ web/viewer.js
@@ -194,3 +194,28 @@ if (document.readyState === 'interactive' ||
 // UI mods
 // Disable right-click context menu for PDF Viewer
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+// // Disable Ctrl + P Printing
+// if ('matchMedia' in window) {
+//     // Chrome, Firefox, and IE 10 support mediaMatch listeners
+//     window.matchMedia('print').addListener(function(media) {
+//         if (media.matches) {
+//             beforePrint();
+//         } else {
+//             // Fires immediately, so wait for the first mouse movement
+//             $(document).one('mouseover', afterPrint);
+//         }
+//     });
+// } else {
+//     // IE and Firefox fire before/after events
+//     $(window).on('beforeprint', beforePrint);
+//     $(window).on('afterprint', afterPrint);
+// }
+// function beforePrint() {
+//     $(".textLayer").hide();
+//     $(".PrintMessage").show();
+// }
+// function afterPrint() {
+//     $(".PrintMessage").hide();
+//     $(".textLayer").show();
+// }
